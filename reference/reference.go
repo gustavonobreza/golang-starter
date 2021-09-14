a8e6c9391606bb77b0955b04fc47df54501546c7
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-09-13 21:23:07 -0300

    feat: add reference studies

---
+++ reference/reference.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	println("Start!!!")
+	str := "Hi, good morning!"
+
+	passByReference(&str)
+
+	passByValue(str)
+}
+
+func passByReference(addStr *string) {
+	myStringAddress := fmt.Sprintf("%p", addStr)
+	println("get address -> " + myStringAddress + " and acessing the address we has \"" + *addStr + "\"")
+}
+
+func passByValue(valueStr string) {
+	myStringAddress := fmt.Sprintf("%p", &valueStr)
+	println("get value -> " + valueStr + " and getting the address we has \"" + myStringAddress + "\"")
+}
