9e3d83cddab8ad42793c8a7a8c0595a084df9856
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-09-13 22:01:19 -0300

    refact(reference): add pipes in logs

--- reference/reference.go
+++ reference/reference.go
@@ -13,10 +13,10 @@ func main() {
 
 func passByReference(addStr *string) {
 	myStringAddress := fmt.Sprintf("%p", addStr)
-	println("get address -> " + myStringAddress + " and acessing the address we has \"" + *addStr + "\"")
+	println("get address -> |" + myStringAddress + "| and acessing the address we has |\"" + *addStr + "\"|")
 }
 
 func passByValue(valueStr string) {
 	myStringAddress := fmt.Sprintf("%p", &valueStr)
-	println("get value -> " + valueStr + " and getting the address we has \"" + myStringAddress + "\"")
+	println("get value -> |" + valueStr + "| and getting the address we has |\"" + myStringAddress + "\"|")
 }
